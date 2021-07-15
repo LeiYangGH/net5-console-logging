f1e7e3a6ff8f66580f0829aa00db81d4d9d1af4c
LeiYangGH <675686066@qq.com>
2021-07-15 13:00:12 +0800

    can log

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder => builder
+                   .AddFilter("Microsoft", LogLevel.Warning)
+                   .AddFilter("System", LogLevel.Warning)
+                   .AddFilter("ConsoleApp1.Program", LogLevel.Debug)
+                   .AddConsole())
+                .BuildServiceProvider();
+
+            //get logger
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogDebug("Starting application");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # net5-console-logging
 test net5 console logging
+
+```bash
+Hello World!
+dbug: ConsoleApp1.Program[0]
+      Starting application
+```
